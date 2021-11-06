cef30f81ceacfcea8ca28f784f4f43cec74b0d31
Alexander Bär <baer.elektro@web.de>
2021-11-06 17:29:55 +0100

    add comments

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -1,25 +1,35 @@
 pragma ton-solidity ^0.51.0;
 
+// Импортируй zombiefactory.sol в новый файл zombiefeeding.sol.
+
 import "./ZombieFactory.sol";
 
+// Интерфейс котиков возвращает ДНА котика по его ID. 
 interface IKittyInterface {
     function getKitty(uint kittyId) external responsible returns (
         uint dna
     );
 }
 
+// Контракт поедания котйков наследует контракт фабрики зоби.
 contract ZombieFeeding is ZombieFactory {
+    
     IKittyInterface kittyContract;
 
+    
     function feedAndMultiply(uint zombieId, uint targetDna) public returns (uint) {
         tvm.log("feedAndMultiply:");
         tvm.hexdump(zombieId);
         tvm.hexdump(targetDna);
+        //Проверяем является ли котик собственностью вызывающрго.
         require(msg.sender == zombieToOwner[zombieId]);
+        // Разтршаем тратиь газ конракта.
         tvm.accept();
+        
         Zombie myZombie = _zombies[zombieId];
         targetDna = targetDna % _dnaModulus;
         uint newDna = (myZombie.dna + targetDna) / 2;
+        
         return _createZombie("NoName", newDna);
     }
 
