67613ca4ca476b95bf4e21bdc7af00b646b05c4b
baerelektro <baer.elektro@web.de>
2022-07-31 09:47:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ set -o errexit
 
 npm install -g tondev
 
-tondev sol update
-tondev network default se
-tondev signer add giver 172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3
-tondev network giver se 0:b5e9240fc2d2f1ff8cbb1d1dee7fb7cae155e5f6320e585fcc685698994a19a5 --signer giver
-tondev signer generate owner
-tondev signer default owner
+everdev sol update
+everdev network default se
+everdev signer add giver 172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3
+everdev network giver se 0:b5e9240fc2d2f1ff8cbb1d1dee7fb7cae155e5f6320e585fcc685698994a19a5 --signer giver
+everdev signer generate owner
+everdev signer default owner
